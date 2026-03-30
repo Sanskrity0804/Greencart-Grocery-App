499568b09b501e6cc6823d650ad78d05ee4793bc
Sanskrity0804 <sanskrityjaiswal@gmail.com>
2026-03-30 18:53:23 +0530

    Updated cart count

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -59,13 +59,34 @@ export const AppContextProvider = ({ children }) => {
         setCartItems(cartData)
     }
 
+    // Get cart item count
+    const getCartCount = () => {
+        let totalCount = 0;
+        for (const item in cartItems) {
+            totalCount += cartItems[item];
+        }
+        return totalCount;
+    }
+
+    // Get cart total amount
+    const getCartAmount = () => {
+        let totalAmount = 0;
+        for (const items in cartItems) {
+            let itemInfo = products.find((product) => product._id === items);
+            if(cartItems[items] > 0) {
+                totalAmount += itemInfo.offerPrice * cartItems[items]
+            }
+        }
+        return Math.floor(totalAmount*100)/100;
+    }
+
     useEffect(()=>{
       fetchProducts()
     },{})
 
     const value = {navigate,user,setUser,setIsSeller,isSeller,
         showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItem,
-        removeFromCart, cartItems, searchQuery, setSearchQuery
+        removeFromCart, cartItems, searchQuery, setSearchQuery, getCartAmount, getCartCount
     }
 
     return <AppContext.Provider value={value}>
