82ae539f8574d076880f0af4df4e5e91e7366fd1
Sanskrity0804 <sanskrityjaiswal@gmail.com>
2026-04-29 02:55:37 +0530

    update stripe payment

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authUser from "../middlewares/authUser.js";
-import { placeOrderCOD, getUserOrders, getAllOrders } from "../controllers/orderController.js";
+import { placeOrderCOD, getUserOrders, getAllOrders, placeOrderStripe } from "../controllers/orderController.js";
 import authSeller from "../middlewares/authSeller.js";
 
 const orderRouter = express.Router();
@@ -8,6 +8,7 @@ const orderRouter = express.Router();
 orderRouter.post('/cod', authUser, placeOrderCOD)
 orderRouter.get('/user', authUser, getUserOrders)
 orderRouter.get('/seller', authSeller, getAllOrders)
+orderRouter.post('/stripe', authUser, placeOrderStripe)
 
 
 export default orderRouter;
