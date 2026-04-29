b05ab487e2a716951b7291a8f006c5ddc8de962d
Sanskrity0804 <sanskrityjaiswal@gmail.com>
2026-04-29 19:54:46 +0530

    fix: case-sensitive import issue

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,4 +1,4 @@
-import Product from "../models/product.js";
+import Product from "../models/Product.js";
 import Order from "../models/Order.js";
 import stripe from "stripe";
 import { request, response } from "express";
--- server/server.js
+++ server/server.js
@@ -12,6 +12,9 @@ import addressRouter from './routes/addressRoute.js';
 import orderRouter from './routes/orderRoute.js';
 import { stripeWebhooks } from './controllers/orderController.js';
 
+const cors = require("cors");
+app.use(cors());
+
 const app = express();
 const port = process.env.PORT || 4000;
 
